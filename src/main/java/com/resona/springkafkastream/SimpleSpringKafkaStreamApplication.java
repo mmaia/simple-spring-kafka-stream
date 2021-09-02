f722c9fd93781de0f42c3f2c6ce33e8bd205b5a0
codespair <codespairams@gmail.com>
2021-09-02 13:51:33 +0200

    initial project

---
+++ src/main/java/com/resona/springkafkastream/SimpleSpringKafkaStreamApplication.java
@@ -0,0 +1,13 @@
+package com.resona.springkafkastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringKafkaStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringKafkaStreamApplication.class, args);
+	}
+
+}
